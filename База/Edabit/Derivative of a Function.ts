050a7c2c96ad11abc5ae74bd40117c16e95b2740
Igor-Ny <82311955+Igor-Ny@users.noreply.github.com>
2021-07-03 14:24:08 +0300

    Delete База directory

--- База/Edabit/Derivative of a Function.ts
+++
@@ -1,6 +0,0 @@
-function derivative(b, m) {
-    let f:number = b*(Math.pow(m, b-1));
-    return f;
-}
-
-console.log(derivative(4, -3));
--- База/база/7_generic.ts
+++
@@ -1,9 +0,0 @@
-const arrayOfNumbers: Array<number> = [1, 1, 2, 3, 5, 8];
-const arrayOfString: Array<string> = ['Hello', 'Igor'];
-
-function reverse<T>(array: T[]): T[] {
-    return array.reverse();
-}
-
-reverse(arrayOfNumbers);
-reverse(arrayOfString);
